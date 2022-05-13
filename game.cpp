8d813c8b130729ba085e00d5094a274c7f73c8da
KhoiSniper <21020770@vnu.edu.vn>
2022-05-13 19:42:06 +0700

    test

--- game.cpp
+++ game.cpp
@@ -3,17 +3,26 @@
 
 
 Game::Game(float width, float height)
-{
+{   
     t1.loadFromFile("image/back6.png");
     t2.loadFromFile("image/ball.png");  
-    t3.loadFromFile("image/paddle2.png");
-    t4.loadFromFile("image/block13.png");
+    t3.loadFromFile("image/paddle3.png");
+    t4.loadFromFile("image/block12.png");
+    taddPaddleWidth.loadFromFile("image/addWidthPad.png");
+    tdoubleBall.loadFromFile("image/doubleBall.png");
 
     back.setTexture(t1);
     ball.setTexture(t2);
+    ball2.setTexture(t2);
+    ball2.setPosition(-100,0);
     ball.setPosition(284,560);
     paddle.setTexture(t3);
-    paddle.setPosition(260,576);
+    paddle.setPosition(260,586);
+    redBall.setTexture(t2);
+    redBall.setColor(sf::Color::Red);
+    addWidthPad.setTexture(taddPaddleWidth);
+    doubleBall.setTexture(tdoubleBall);
+   
     int k=0;
     for(int i=5;i<15;i++)
     {
@@ -24,12 +33,26 @@ Game::Game(float width, float height)
          k++;
         }
     }
+    s=rand()%100+80;
+    t=rand()%80+40;
+    w=rand()%40;
+    redBall.setPosition(brick[s].getPosition().x,brick[s].getPosition().y);
+    doubleBall.setPosition(brick[t].getPosition().x,brick[t].getPosition().y);
+    addWidthPad.setPosition(brick[w].getPosition().x,brick[w].getPosition().y);
     
     scoreText.setCharacterSize(30);
     font.loadFromFile("Font/font1.otf");
     scoreText.setFont(font);
     scoreText.setFillColor(sf::Color::Red);
     scoreText.setPosition(0,0);
+    scoreText.setString("Score: ");
+    
+    toNum.setCharacterSize(30);
+    font.loadFromFile("Font/font1.otf");
+    toNum.setFont(font);
+    toNum.setFillColor(sf::Color::Yellow);
+    toNum.setPosition(0,40);
+    toNum.setString(to_string(scoreNum));
 
 }
 
@@ -38,12 +61,15 @@ void Game::draw(sf::RenderWindow &window)
   window.draw(back);
   window.draw(ball);
   window.draw(paddle);
+  window.draw(redBall);
+  window.draw(doubleBall);
+  window.draw(addWidthPad);
   for(int i=0;i<n;i++)
   {
       window.draw(brick[i]);
   }
-  window.draw(redBall);
   window.draw(scoreText);
+  window.draw(toNum);
 
 }
 
@@ -54,23 +80,94 @@ void Game::EventProcess()
             {
                 if(isCollide(ball,brick[i])==true)
                     {
-                        brick[i].setPosition(-100,0);dx=-dx;
+                        brick[i].setPosition(-100,0);
+                        if(ball.getColor()!=sf::Color::Red) dx=-dx;
+                            if(i==s)
+                            {
+                                isRedBallTouch=true;
+                            }
+                            if(i==t)
+                            {
+                                isDoubleBallTouch=true;
+                            }
+                            if(i==w)
+                            {
+                                isaddWidthTouch=true;
+                            }
                     }
             }
             
+            if(isRedBallTouch==true)
+            {
+                redBall.move(0,0.75);
+            }
+            if(isaddWidthTouch==true)
+            {
+                addWidthPad.move(0,0.75);
+            }
+            if(isDoubleBallTouch==true)
+            {
+                doubleBall.move(0,0.75);
+            }
+           
+
             ball.move(0,dy);
             for(int i=0;i<n;i++)
                 {
                 if(isCollide(ball,brick[i])==true)
                     {
-                        brick[i].setPosition(-100,0);dy=-dy;
-                    }
-                }               
+                        brick[i].setPosition(-100,0);
+                        if(ball.getColor()!=sf::Color::Red) dy=-dy;  
+                
+                          if(i==s)
+                            {
+                                isRedBallTouch=true;
+                            }
+                            if(i==t)
+                            {
+                                isDoubleBallTouch=true;
+                            }
+                            if(i=w)
+                            {
+                                isaddWidthTouch=true;
+                            }        
+                    }               
+                }
             
-                if(isCollide(ball,paddle)==true)
+            if(isRedBallTouch==true)
+            {
+                redBall.move(0,0.75);
+            }
+            if(isaddWidthTouch==true)
+            {
+                addWidthPad.move(0,0.75);
+            }
+            if(isDoubleBallTouch==true)
+            {
+                doubleBall.move(0,0.75);
+            }
+//Ktra paddle co cham duoc item khong            
+            if(isCollide(redBall,paddle))
+            {
+                ball.setColor(sf::Color::Red);
+                redBall.setPosition(-100,0);
+            }
+            if(isCollide(addWidthPad,paddle))
+            {
+                paddle.setScale(1.5,1);
+                addWidthPad.setPosition(-100,0);
+            }
+            if(isCollide(doubleBall,paddle))
+            {
+                ball2.setPosition(ball.getPosition().x,ball.getPosition().y);
+                doubleBall.setPosition(-100,0);
+            }
+            
+            if(isCollide(ball,paddle)==true)
                     {
                         dy=-(rand()%3+2);
                     }
+
             sf::Vector2f g=ball.getPosition();
             if(g.x<50||g.x>550-16) dx=-dx; 
             if(g.y<0)    dy=-dy;
@@ -80,17 +177,21 @@ void Game::EventProcess()
             }
 
 
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) paddle.move(10,0);
-            if(paddle.getPosition().x>550-80) paddle.setPosition(550-80-4,576);
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) paddle.move(-10,0);
-            if(paddle.getPosition().x<50) paddle.setPosition(50,576);
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) paddle.move(6,0);
+            if((paddle.getPosition().x+paddle.getGlobalBounds().width)>550)
+            {
+                paddle.setPosition(550-paddle.getGlobalBounds().width-3,586);
+            }
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) paddle.move(-6,0);
+            if(paddle.getPosition().x<50) paddle.setPosition(51,586);
 }
 
 void Game::run()
 {
     sf::RenderWindow window(sf::VideoMode(600,600),"Game");
-    window.setFramerateLimit(60);
+    window.setFramerateLimit(120);
     Game game(window.getSize().x,window.getSize().y);
+    
     while(window.isOpen())
     {
         sf::Event e;
@@ -121,8 +222,4 @@ bool Game::isCollide(sf::Sprite s1, sf::Sprite s2)
     return false;
 }
 
-void Game::update()
-{
-    
-}
 
--- game.h
+++ game.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <sstream>
 #include <iostream>
 using namespace std;
@@ -9,19 +10,22 @@ class Game
 public:
         Game(float width,float height);
         sf::RenderWindow window;
-        sf::Texture t1,t2,t3,t4,tball,taddPaddleWidth;
-        sf::Sprite back,ball,paddle,brick[1000],redBall,addPaddleWidth;
+        sf::Texture t1,t2,t3,t4,tball,tdoubleBall,taddPaddleWidth;
+        sf::Sprite back,ball,paddle,brick[1000],redBall,addWidthPad,doubleBall,ball2;
         void draw(sf::RenderWindow &window);
         void EventProcess();
         bool isCollide(sf::Sprite s1,sf::Sprite s2);
         void run();
-        void update();
         void isGameStop();
         bool isStop=false;
+        bool isRedBallTouch=false;
+        bool isaddWidthTouch=false;
+        bool isDoubleBallTouch=false;
         int n=100;
-        int s=rand()%100;
-        float dx=-4,dy=-5;
+        int s,t,w;
+        float dx=-2,dy=-3;
         sf::Text scoreText;
+        sf::Text toNum;
         int scoreNum=0;
         sf::Font font;
 };
